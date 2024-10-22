fed66c8c8f1e6ba28b49415df1e73cf8ee1e4db8
Xudoynazar1112 <141805748+Xudoynazar1112@users.noreply.github.com>
2024-10-22 18:29:03 +0500

    updated NavLinks

--- src/global/Navbar.jsx
+++ src/global/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div className='flex items-center justify-between py-7'>
       <div>
         <ul className='flex gap-10 items-center text-white'>
-          <li><a href="/"><img src="/icons/header__icon.png" alt="logo" className='w-36' /></a></li>
-          <li><a href="/">Uy</a></li>
-          <li><a href="/shop">To'plam</a></li>
-          <li><a href="/about">Biz Haqimizda</a></li>
-          <li><a href="/contact">Aloqa</a></li>
+          <li><NavLink to="/"><img src="/icons/header__icon.png" alt="logo" className='w-36' /></NavLink></li>
+          <li><NavLink to="/">Uy</NavLink></li>
+          <li><NavLink to="/shop">To'plam</NavLink></li>
+          <li><NavLink to="/about">Biz Haqimizda</NavLink></li>
+          <li><NavLink to="/contact">Aloqa</NavLink></li>
         </ul>
       </div>
       <div className='mr-20'>
