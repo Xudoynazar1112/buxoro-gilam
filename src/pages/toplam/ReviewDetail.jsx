19d2a409f9e682c6bd75cc993220bd518033d17e
Xudoynazar1112 <141805748+Xudoynazar1112@users.noreply.github.com>
2024-10-27 21:23:57 +0500

    review jsx

---
+++ src/pages/toplam/ReviewDetail.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+
+const ReviewDetail = () => {
+    const {t, i18n} = useTranslation()
+  return (
+    <div className='grid grid-cols-2'>
+        <div>
+        <form></form>
+        </div>
+        <div></div>
+    </div>
+  )
+}
+
+export default ReviewDetail
